8f5808350813634eada550ace20efe2131898d4a
kshiitij <kshiitijsrivastava20@gmail.com>
2025-01-24 16:27:14 +0530

    bae mapsssss

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -1,31 +1,55 @@
 <template>
 <div class="LeftSideBar">
+    
     <v-main style="--v-layout-top: 0px;">
+        <v-app-bar color="rgb(2, 42, 56)" height="140" elevation="0"></v-app-bar>
+        <v-navigation-drawer rail color="rgb(2, 42, 56)" location="right"></v-navigation-drawer>
         <v-navigation-drawer rail color="rgb(2, 42, 56)">
             <v-list>
                 <!-- Layers Button -->
                 <v-list-item class="cursor-pointer" @click="toggleDrawer">
-                    <v-icon style="color: wheat;">mdi-heart</v-icon>
+                    <v-icon style="color: wheat;">mdi-layers-triple</v-icon>
                 </v-list-item>
             </v-list>
         </v-navigation-drawer>
     </v-main>
-
+    
     <v-navigation-drawer v-model="drawer" temporary>
-        <v-card elevation="0">
-            <v-card-title>
-                <div class="text-h5" style="font-family: 'Poppins', sans-serif; font-weight:500; text-align: center;">Feature Info Tool</div>
-            </v-card-title>
+        <v-list>
+            <v-list-item>
+                <div class="text-h5" style="font-family: 'Poppins', sans-serif; font-weight:500; text-align: center;">Layers</div>
+            </v-list-item>
             <v-divider :thickness="2" color="black"></v-divider>
-            <v-card-text>
-                <v-switch color="error" v-model="featureInfoEnabled" label="Enable Feature Info" @change="toggleFeatureInfo"></v-switch>
-            </v-card-text>
-        </v-card>
-        <v-divider :thickness="2" color="black"></v-divider>
+            <v-expansion-panels>
+                <v-expansion-panel elevation="0">
+                    <v-expansion-panel-title>Administrative Boundaries</v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <!-- Boundary Layer Checkboxes -->
+                        <v-list-item v-for="(adminLayer, index) in adminLayers" :key="index">
+                            <v-checkbox color="primary" :label="adminLayer.name" v-model="adminLayer.visible" style="margin-top: -7px; margin-bottom: -38px; margin-left:-10px" @change="onAdminBoundaryVisibilityChange(adminLayer)"></v-checkbox>
+                        </v-list-item>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+                <v-expansion-panel elevation="0">
+                    <v-expansion-panel-title>
+                        Project Boundaries
+                    </v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <!-- Project Layer Checkboxes -->
+                        <v-list-item v-for="(projectLayer, index) in projectLayers" :key="index">
+                            <v-checkbox color="primary" :label="projectLayer.name" v-model="projectLayer.visible" style="margin-top: -7px; margin-bottom: -38px; margin-left:-10px" @change="onProjectBoundaryVisibilityChange(projectLayer)"></v-checkbox>
+                        </v-list-item>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
+            <v-divider></v-divider>
+        </v-list>
     </v-navigation-drawer>
 </div>
 </template>
 
+  
+  
 <script>
 import eventBus from '@/event-bus';
 
@@ -33,20 +57,38 @@ export default {
     name: 'LeftBar',
     data: () => ({
         drawer: false,
-        featureInfoEnabled: false,
+        adminLayers: [{
+                name: 'India Boundary',
+                visible: true
+            },
+
+        ],
+        projectLayers: [{
+            name: 'Ganga Basin',
+            visible: true
+        }, ],
     }),
     methods: {
         toggleDrawer() {
             this.drawer = !this.drawer;
         },
-        toggleFeatureInfo() {
-            eventBus.emit('toggleFeatureInfo', this.featureInfoEnabled);
+        onAdminBoundaryVisibilityChange(adminLayer) {
+            eventBus.emit('toggle-layer-visibility', {
+                name: adminLayer.name,
+                visible: adminLayer.visible,
+            });
         },
-
+        onProjectBoundaryVisibilityChange(projectLayer) {
+            eventBus.emit('toggle-layer-visibility', {
+                name: projectLayer.name,
+                visible: projectLayer.visible,
+            });
+        },
     },
 };
 </script>
-
+  
+  
 <style scoped>
-
+/* No changes needed for styling */
 </style>
--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,196 +1,175 @@
 <template>
-    <div>
-      <div id="map" ref="mapContainer" style="width: 100%; height: 100vh;"></div>
-      <div id="popup" ref="popup" class="ol-popup" v-show="popupContent">
-        <div v-html="popupContent"></div>
+    <div id="map" ref="mapContainer" style="position: relative; width: 100%; height: 85vh;">
+      <div class="baseSwitcher">
+        <v-menu location="top">
+      <template v-slot:activator="{ props }">
+        <v-btn color="rgb(2, 42, 56)" dark v-bind="props"><div style="color: wheat;">Base Maps</div></v-btn>
+      </template>
+  <v-list>
+    <v-list-item v-for="(basemap, index) in basemaps" :key="index" class="mx-2" @click="switchBasemap(basemap.name)">
+        <v-img :src="basemap.icon" alt="Basemap Option" contain max-width="900" max-height="300" />
+      <v-list-item-title>{{ basemap.label }}</v-list-item-title>
+    </v-list-item>
+  </v-list>
+</v-menu>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import 'ol/ol.css';
-  import { Map, View } from 'ol';
-  import TileLayer from 'ol/layer/Tile';
-  import BingMaps from 'ol/source/BingMaps';
-  import TileWMS from 'ol/source/TileWMS';
-  import Overlay from 'ol/Overlay';
-  import { get as getProjection } from 'ol/proj';
-  import eventBus from '@/event-bus';
-  
-  export default {
+  </template> 
+ 
+<script>
+import 'ol/ol.css';
+import {Map,View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import BingMaps from 'ol/source/BingMaps';
+import TileWMS from 'ol/source/TileWMS';
+import OSM from 'ol/source/OSM';
+import {get as getProjection} from 'ol/proj';
+import eventBus from '@/event-bus';
+
+export default {
     name: 'MapComponent',
     data() {
-      return {
-        map: null,
-        layers: [], // Store all WMS layers
-        popupContent: null, // Content for the popup
-        popup: null, // Overlay for the popup
-        featureInfoEnabled: false, // Controlled by the event bus
-      };
+        return {
+            map: null,
+            layers: [],
+      basemaps: [
+        { name: 'osm', label: 'Open Street Map', icon: require('@/assets/images/openstreetmap.png') },
+        { name: 'bing', label: 'Bing Map', icon: require('@/assets/images/bing.png') },
+        { name: 'bhuvan', label: 'Bhuvan Satellite Imagery', icon: require('@/assets/images/bhuvan.png') },
+      ],
+          
+      
+        };
     },
     mounted() {
-      this.$nextTick(() => {
-        // Initialize popup overlay
-        this.popup = new Overlay({
-          element: this.$refs.popup,
-          positioning: 'bottom-center',
-          stopEvent: false,
+        // Define layers
+        const osm = new TileLayer({
+            title: 'osm',
+            type: 'base',
+            source: new OSM(),
+            visible: true,
         });
-  
-        // Add popup to map
-        this.map.addOverlay(this.popup);
-      });
-  
-      // Listen for feature info toggle from event bus
-      eventBus.on('toggleFeatureInfo', (isEnabled) => {
-        this.featureInfoEnabled = isEnabled;
-      });
-  
-      // Define layers
-      const bing = new TileLayer({
-        title: 'Bing',
-        type: 'base',
-        source: new BingMaps({
-          key: 'ArIdKOW0eb8TRcLZdt0l2cG8kHA_uW92yIvx1aFzsQ1xHxpnVRMGmO0N0neY8P90',
-          imagerySet: 'AerialWithLabelsOnDemand',
-        }),
-        visible: true,
-      });
-  
-      const basinBoundary = new TileLayer({
-        title: 'Ganga Basin',
-        type: 'overlay',
-        source: new TileWMS({
-          url: 'http://192.168.17.37:8080/geoserver/Geo-Ganga/wms?',
-          params: {
-            LAYERS: 'Ganga_Basin_v4',
-            TILED: true,
-            VERSION: '1.1.1',
-          },
-          serverType: 'geoserver',
-          tileGrid: new TileWMS().getTileGridForProjection(
-            getProjection('EPSG:4326')
-          ),
-        }),
-        visible: true,
-      });
-  
-      const SubDistrictsBoundary = new TileLayer({
-        title: 'Sub Districts',
-        type: 'overlay',
-        source: new TileWMS({
-          url: 'http://192.168.17.37:8080/geoserver/Geo-Ganga/wms?',
-          params: {
-            LAYERS: 'subdistrict_touch',
-            TILED: true,
-            VERSION: '1.1.1',
-          },
-          serverType: 'geoserver',
-          tileGrid: new TileWMS().getTileGridForProjection(
-            getProjection('EPSG:4326')
-          ),
-        }),
-        visible: true,
-      });
-  
-      // Add layers to an array for easier management
-      this.boundary = [basinBoundary, SubDistrictsBoundary];
-  
-      // Initialize the map
-      this.map = new Map({
-        target: 'map',
-        layers: [bing, ...this.boundary],
-        view: new View({
-          projection: 'EPSG:4326',
-          center: [82.0662, 26.2648],
-          zoom: 6.5,
-          minZoom: 6.5,
-          maxZoom: 19,
-          extent: [68.1, 6.46, 97.4, 37.09],
-        }),
-      });
-  
-      // Add click event for feature info
-      this.map.on('singleclick', this.handleMapClick);
-    },
-    methods: {
-      async handleMapClick(event) {
-        if (!this.featureInfoEnabled) return; // Skip if feature info is disabled
-  
-        const coordinate = event.coordinate;
-        const resolution = this.map.getView().getResolution();
-        let featureInfoResults = [];
-  
-        // Loop through all visible WMS layers
-        for (const layer of this.boundary) {
-          if (layer.getVisible()) {
-            const wmsSource = layer.getSource();
-            const url = wmsSource.getFeatureInfoUrl(
-              coordinate,
-              resolution,
-              'EPSG:4326',
-              { INFO_FORMAT: 'application/json' }
-            );
-  
-            if (url) {
-              try {
-                const response = await fetch(url);
-                const data = await response.json();
-  
-                if (data.features && data.features.length > 0) {
-                  featureInfoResults = featureInfoResults.concat(
-                    data.features.map((feature) => ({
-                      layer: layer.get('title'),
-                      properties: feature.properties,
-                    }))
-                  );
-                }
-              } catch (error) {
-                console.error(
-                  `Error fetching feature info for layer '${layer.get(
-                    'title'
-                  )}':`,
-                  error
-                );
-              }
+        const bing = new TileLayer({
+            title: 'Bing',
+            type: 'base',
+            source: new BingMaps({
+                key: "ArIdKOW0eb8TRcLZdt0l2cG8kHA_uW92yIvx1aFzsQ1xHxpnVRMGmO0N0neY8P90",
+                imagerySet: 'AerialWithLabelsOnDemand',
+            }),
+            visible: false,
+        });
+        const bhuvan = new TileLayer({
+            title: 'Bhuvan',
+            type: 'base',
+            source: new TileWMS({
+                params: {
+                    FORMAT: "image/jpeg",
+                    VERSION: "1.1.1",
+                    tiled: true,
+                    LAYERS: "bhuvan_imagery",
+                    exceptions: "application/vnd.ogc.se_inimage",
+                },
+                url: "https://bhuvan-ras2.nrsc.gov.in/tilecache/tilecache.py",
+            }),
+            visible: false,
+        });
+
+        this.baseMaps = [bhuvan, bing, osm];
+
+        // Other overlays (India and Ganga Basin boundaries)
+        const indiaCountryBoundary = new TileLayer({
+            name: 'India',
+            type: 'overlay',
+            source: new TileWMS({
+                url: 'http://192.168.17.37:8080/geoserver/Geo-Ganga/wms?',
+                params: {
+                    'LAYERS': 'india_country_boundary_4326',
+                    'TILED': true,
+                    'VERSION': '1.1.1'
+                },
+                serverType: 'geoserver',
+                tileGrid: new TileWMS().getTileGridForProjection(getProjection('EPSG:4326')),
+            }),
+            visible: true,
+        });
+
+        const basinBoundary = new TileLayer({
+            name: 'Ganga Basin',
+            type: 'overlay',
+            source: new TileWMS({
+                url: 'http://192.168.17.37:8080/geoserver/Geo-Ganga/wms?',
+                params: {
+                    LAYERS: 'Ganga_Basin_v4',
+                    TILED: true,
+                    VERSION: '1.1.1'
+                },
+                serverType: 'geoserver',
+                tileGrid: new TileWMS().getTileGridForProjection(getProjection('EPSG:4326')),
+            }),
+            visible: true,
+        });
+
+        this.boundaries = [basinBoundary, indiaCountryBoundary];
+
+        // Initialize the map
+        this.map = new Map({
+            target: 'map',
+            layers: [...this.baseMaps, ...this.boundaries],
+            view: new View({
+                projection: 'EPSG:4326',
+                center: [82.0662, 26.2648],
+                zoom: 6.5,
+                minZoom: 6.5,
+                maxZoom: 19,
+                extent: [68.1, 6.46, 97.4, 37.09],
+            }),
+        });
+
+        this.layers = {
+            'Ganga Basin': basinBoundary,
+            'India Boundary': indiaCountryBoundary,
+        };
+
+        eventBus.on('toggle-layer-visibility', ({
+            name,
+            visible
+        }) => {
+            const layer = this.layers[name];
+            if (layer) {
+                layer.setVisible(visible);
+            } else {
+                console.log(`"${name}" not found`);
             }
-          }
-        }
-  
-        // Update popup content and position
-        if (featureInfoResults.length > 0) {
-          this.popupContent = featureInfoResults
-            .map(
-              (result) => `
-            <strong>Layer: ${result.layer}</strong><br/>
-            ${Object.entries(result.properties)
-              .map(([key, value]) => `<b>${key}:</b> ${value}`)
-              .join('<br/>')}
-          `
-            )
-            .join('<hr/>');
-  
-          this.popup.setPosition(coordinate);
-        } else {
-          this.popup.setPosition(undefined); // Hide the popup
-          this.popupContent = null; // Clear content
-        }
-      },
+        });
     },
-  };
-  </script>
+
+    methods: {
+        switchBasemap(baseMapName) {
+      // Set the visibility of basemaps
+      this.baseMaps.forEach((layer) => {
+        layer.setVisible(layer.get('title').toLowerCase() === baseMapName.toLowerCase());
+      });
+
+      // Update the current basemap icon
+      const selectedBasemap = this.basemaps.find(
+        (basemap) => basemap.name.toLowerCase() === baseMapName.toLowerCase()
+      );
+      if (selectedBasemap) {
+        this.currentBasemapIcon = selectedBasemap.icon;
+      }
+    },
+  },
+};
+</script>
+
   
-  <style scoped>
-  .ol-popup {
-    position: absolute;
-    background: white;
-    border: 1px solid #ccc;
-    padding: 10px;
-    border-radius: 4px;
-    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
-    min-width: 150px;
-    pointer-events: none;
-    transform: translateY(-10px);
-  }
-  </style>
   
+<style scoped>
+.baseSwitcher{
+    position:absolute;
+    z-index: 2;
+    bottom: 5%;
+    right: .5%;
+}
+</style>
+
